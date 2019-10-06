3cb9add89b4bbb0d785cab39e96b9223bcb38ec7
lytc <prtran@gmail.com>
2019-10-07 03:08:02 +0700

    update doc build

--- packages/react-sortly/src/useAnimationFrame.ts
+++ packages/react-sortly/src/useAnimationFrame.ts
@@ -1,19 +1,5 @@
 import React from 'react';
 
-// // @ts-ignore
-// let stats;
-// if (process.env.NODE_ENV === 'development') {
-//   // @ts-ignore
-//   stats = new global.Stats();
-//   stats.showPanel(0);
-//   stats.dom.style.opacity = 0.3;
-//   stats.dom.style.transition = 'opacity 0.5s';
-//   stats.dom.style.top = '1px';
-//   stats.dom.style.right = '1px';
-//   stats.dom.style.left = 'auto';
-//   document.body.appendChild(stats.dom);
-// }
-
 const useAnimationFrame = (callback: () => void) => {
   const requestRef = React.useRef<number>();
   const cancel = () => {
@@ -24,10 +10,6 @@ const useAnimationFrame = (callback: () => void) => {
   };
   const animate = () => {
     callback();
-    // if (process.env.NODE_ENV === 'development') {
-    //   // @ts-ignore
-    //   stats.update(); // eslint-disable-line no-undef
-    // }
     requestRef.current = requestAnimationFrame(animate);
   };
 
