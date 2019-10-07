efbc21ce1546e6798f06c807cf034028bd1cbc2c
lytc <prtran@gmail.com>
2019-10-07 09:46:22 +0700

    cleanup

--- packages/documentation/src/index.tsx
+++ packages/documentation/src/index.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-// @ts-ignore
-// import { registerObserver } from 'react-perf-devtool';
 
 import App from './App';
-
-// registerObserver();
-
-// type Foo = {
-//   id: number;
-// };
-
-// const App = () => <div>xxxx</div>;
 
 const root = document.createElement('div');
 document.body.appendChild(root);
